88aa2c1c760f9396964e228d8eb691b39a893255
Anderson Ayala Vera <140223850+AAV1102@users.noreply.github.com>
2023-08-07 22:40:16 -0500

    Delete Otros directory

--- Otros/cuentaRegresiva.js
+++
@@ -1,9 +0,0 @@
-function cuentaRegresiva(initial){
-    let numeros = [];
-    for(let i = 0; i<=initial; i++){
-        numeros.push(i)
-    }
-    return numeros.reverse()
-}
-
-console.log(cuentaRegresiva(4))
