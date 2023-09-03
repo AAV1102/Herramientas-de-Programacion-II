e48b7d5b4b5755a0dce62c0044aeebf3075a299a
Anderson Ayala Vera <140223850+AAV1102@users.noreply.github.com>
2023-09-03 18:54:20 -0500

    Delete Actividad 21 Julio 2023 directory

--- Actividad 21 Julio 2023/numerospositivos.js
+++
@@ -1,8 +0,0 @@
-const temperaturas = [25, 30, 28, 32, 29, 27];
- let mayorTemperatura = 0;
- 
- for (let i = 0; i < temperaturas.length; i++) {
-  if (temperaturas[i] > mayorTemperatura) {
-    mayorTemperatura = temperaturas[i];
-   }
- }
--- Actividad 21 Julio 2023/temperaturas.js
+++
@@ -1,8 +0,0 @@
-let temperaturas =[25, 30, 28, 32, 29, 27];
-let mayor = temperaturas[0];
-for (let index = 0; index < temperaturas.length; index++) {
-    if (temperaturas[index] > mayor) {
-        mayor = temperaturas[index];
-    }
-}
-console.log(mayor);
